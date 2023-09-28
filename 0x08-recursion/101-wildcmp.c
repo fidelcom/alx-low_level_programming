f562f50620c88ceecd69c583acadb9add284f628
fidelcom <okpara2040@gmail.com>
2023-09-28 15:23:14 +0000

    Write a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int substring_match(char *s1, char *s2, char *after_wldcd);
 /**
  * wildcmp - compare if string with wildcard mattches
  * @s1: string to compare
@@ -9,28 +10,34 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-	{
 		return (1);
-	}
-	if (*s2 == '*')
-	{
-		while (*(s2 + 1) == '*')
-		{
-			s2++;
-		}
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
-		}
-		return wildcmp(s1, s2 + 1);
-	}
-		if (*s1 == *s2 || *s2 == '?')
-		{
-			return (wildcmp(s1 + 1, s2 + 1));
-		}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+	else
 		return (0);
+}
+
+/**
+ * substring_match - check if a substring after wildcard matches s1
+ * @s1: one string
+ * @s2: one string
+ * @after_wldcd: placeholder for position right after wildcard
+ * Return: 1 if matched, 0 if not
+ */
+int substring_match(char *s1, char *s2, char *after_wldcd)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
+	if (*s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+	else
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
 }
