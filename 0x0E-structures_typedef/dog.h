6cd4101826e3bd90fbd6dac552f0dbce32e32982
fidelcom <okpara2040@gmail.com>
2023-10-10 17:01:57 +0000

    Define a new type struct dog with the following elements:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type struck
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
