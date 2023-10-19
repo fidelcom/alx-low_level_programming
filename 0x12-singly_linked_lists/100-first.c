a0836d6e52493755bdd422250a23aca69abf6b72
fidelcom <okpara2040@gmail.com>
2023-10-19 16:40:17 +0000

    Write a function that prints  before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+void first_print(void) __attribute__((constructor));
+/**
+* printFirst - function that prints before main is exectuted
+*/
+
+void first_print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
